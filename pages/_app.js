fd731d941f96a98e11eda6b3c542435d7aa396d1
Blaine F Garrett <blaine@blainegarrett.com>
2018-12-29 10:36:04 -0600

    Page Transitions....

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,32 @@ import App, { Container } from 'next/app';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import JssProvider from 'react-jss/lib/JssProvider';
+import { PageTransition } from 'next-page-transitions';
 import getPageContext from '../src/theming/context';
 import GlobalStyles from '../src/theming/GlobalStyles';
 import { Provider as ReduxProvider } from 'react-redux';
 import withReduxStore from '../src/redux/withReduxStore';
 
+import Loader from '../src/components/layout/Loader';
+const TIMEOUT = 500;
+
 class MyApp extends App {
+  pageContext = null;
+
   constructor(props) {
     super(props);
     this.pageContext = getPageContext();
   }
 
-  pageContext = null;
+  static async getInitialProps ({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
 
   componentDidMount() {
     // Remove the server-side injected CSS.
@@ -46,10 +60,52 @@ class MyApp extends App {
                 to render collected styles on server side. */}
 
             <ReduxProvider store={reduxStore}>
-              <Component pageContext={this.pageContext} {...pageProps} />
+              <PageTransition
+                timeout={TIMEOUT}
+                classNames='page-transition'
+                loadingComponent={<Loader />}
+                loadingDelay={500}
+                loadingTimeout={{
+                  enter: TIMEOUT,
+                  exit: 0
+                }}
+                loadingClassNames='loading-indicator'
+              >
+                <Component pageContext={this.pageContext} {...pageProps} />
+              </PageTransition>
             </ReduxProvider>
           </MuiThemeProvider>
         </JssProvider>
+
+
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+            transform: translate3d(0, 20px, 0);
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transform: translate3d(0, 0, 0);
+            transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity ${TIMEOUT}ms;
+          }
+          .loading-indicator-appear,
+          .loading-indicator-enter {
+            opacity: .90;
+          }
+          .loading-indicator-appear-active,
+          .loading-indicator-enter-active {
+            opacity: 1;
+            transition: opacity ${TIMEOUT}ms;
+          }
+        `}</style>
+
       </Container>
     );
   }
